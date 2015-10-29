f9e54b265243b4359bc658f1f29003fc830a5ff0
fernandomv3 <fernandomv3@gmail.com>
2015-10-29 11:58:13 -0500

    added loading csv files

--- cuack.py
+++ cuack.py
@@ -4,6 +4,7 @@ import sys
 import os
 import json
 import getopt
+import csv
 
 template_dir = ""
 jinja_env = None
@@ -12,12 +13,20 @@ dev = False
 def load_template(input_file):
   return jinja_env.get_template(input_file)
 
+def load_csv_values(csv_file,csv_params):
+  with open(csv_file, newline='') as f:
+    reader = csv.DictReader(f,**csv_params)
+    data = []
+    for row in reader:
+      data.append(row)
+    return {'fieldnames': reader.fieldnames, 'data': data}
+
 def write_page(output_file,text):
   with open(output_file,'w') as f:
     f.write(text)
 
 def usage():
-  print("Usage: renderTemplates.py\n")
+  print("Usage: python3 cuack.py\n")
 
 def parse_args():
   global dev
@@ -53,8 +62,11 @@ def main(argv):
   pages = json_conf['pages']
   for page in pages:
     template = load_template(page['template'])
+    for csv_conf in page['csv']:
+      csv_data = load_csv_values(csv_conf['file_name'],csv_conf['csv_params'])
+      page['values'][csv_conf['var_name']] = csv_data
     result= template.render(name=page['name'],dev= dev,**page['values'])
-    write_page(json_conf['output_dir'] + page['name'],result)
+    write_page(json_conf['output_dir']+ "/" + page['name'],result)
 
 if __name__ == "__main__":
   main(sys.argv)
